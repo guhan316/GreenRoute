eefafee837699637091f4e5445183ee2bcfa7572
Guhan R <workguhan@gmail.com>
2026-08-26 19:25:04 +0530

    feat: load cloud dashboard styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
 import './milestone2.css'
+import './cloud.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
